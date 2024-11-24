1993213005e2c8322913b9c4afa4f7bf8a4cb5cd
BURNERN <87552102+burnern1@users.noreply.github.com>
2024-11-24 23:34:24 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.py
@@ -1,5 +1,2 @@
 def solution(my_string, overwrite_string, s):
-    left_part = my_string[:s]
-    right_part = my_string[s + len(overwrite_string):]
-    answer = left_part + overwrite_string + right_part
-    return answer
+    return my_string[:s] + overwrite_string + my_string[s + len(overwrite_string):]
