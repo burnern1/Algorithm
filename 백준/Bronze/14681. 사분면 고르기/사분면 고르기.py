47c4e34b9b22897af307e710df0e5993d00f61db
BURNERN <87552102+burnern1@users.noreply.github.com>
2024-12-05 22:08:53 +0900

    [Bronze V] Title: 사분면 고르기, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -0,0 +1,16 @@
+def find_quadrant(x, y):
+    if x > 0 and y > 0:
+        return 1
+    elif x < 0 and y > 0:
+        return 2
+    elif x < 0 and y < 0:
+        return 3
+    elif x > 0 and y < 0:
+        return 4
+
+
+x = int(input())
+y = int(input())
+
+
+print(find_quadrant(x, y))
